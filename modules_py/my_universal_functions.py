93637dae5c7c569d51e1c551d988f3fc7ec5ab56
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-07-18 11:16:06 -0300

    Add files via upload

--- modules_py/my_universal_functions.py
+++ modules_py/my_universal_functions.py
@@ -10,11 +10,36 @@ def checkIfNotNumeric(*args):
             return False
     return True
 
+def checkIfNotNumericList(L):
+    for x in L:
+        if not(isinstance(x,(int,float))):
+            return False
+    return True
+
+def swapValues(L,idx1,idx2):
+    tmp = L[idx1]
+    L[idx1] = L[idx2]
+    L[idx2] = tmp
+    return L
+
 def addAllNumerics(*args):
     s= 0
     for x in args:
         s+=x
     return s
 
-myName = "Franco Ortiz"
+def findMin(L,startIndx):
+    m = L[startIndx]
+    idx = startIndx
+    for i in range(startIndx,len(L)):
+        x = L[i] #zero
+        if x<m:
+            m = x
+            idx = i #zero
+        else:
+            pass
+    return m,idx
+
+def myName()
+print( "Franco Ortiz")
 
